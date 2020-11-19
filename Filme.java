bfdd11effd2068e28acedf1dc8af40858fa7b390
neocir17 <71474496+neocir17@users.noreply.github.com>
2020-11-18 21:29:08 -0300

    Create Filme.java

---
+++ Filme.java
@@ -0,0 +1,88 @@
+public class Filme {
+       // Atributos
+       private int duracao;
+       private String idiomaAudio;
+       private String idiomaLegenda;
+       private boolean estaPausado;
+       private String nome;
+       private int autor;
+   
+       // metodos construtores
+       public Filme(String nomeFilme, int duracaoFilme, String autorFilme ){
+           this.nome = nomeFilme;
+           this.duracao = duracaoFilme;
+           this.autor = autorFilme;
+           this.idiomaAudio = "Portugês(Brasil)";
+           this.idiomaLegenda = "Portugês(Brasil)";
+           this.estaPausado = false;
+       }
+   
+       // metodos especifico da classe 
+       public void execultar(){
+           if(estaPausado == false){
+               System.out.println("Iniciou o filme: "+ getNome());
+           }else{
+               System.out.println("Processando");
+           }
+       }
+   
+       public void pausar(){
+           estaPausado = true; 
+           System.out.println("O filme está pausado.");
+           }
+           
+           public void mudarAudio(){
+               if(idiomaAudio == "Inglês(Original)"){
+                   idiomaAudio = "Português(Brasil)";
+               }else{
+                   idiomaAudio = "Inglês(Original)";
+               }    
+               
+               System.out.println("Idioma alterado para: " + idiomaAudio);
+           
+           }
+   
+           public void mudarLegenda(){
+               if(idiomaLegenda == "Filme sem legenda"){
+                   idiomaLegenda = "Português(Brasil)";
+               }else{
+                   idiomaLegenda = "Filme sem legenda";
+               }
+               System.out.println("Legenda alterada para: "+ idiomaLegenda);
+           }
+             
+           public void exibirDetalhes(){
+            System.out.println("Detalhes do filme: " + getNome() + " Autor do Filme: " + getAutor() + " Duracao do Filme: " + getDuracao() + " Minutos" );
+           }
+       
+       
+       
+           // Getter e setters
+       
+           public int getDuracao() {
+               return duracao;
+           }
+       
+           public void setDuracao(int duracaoFilme) {
+               this.duracao = duracaoFilme;
+           }
+       
+           public String getNome() {
+               return nome;
+           }
+       
+           public void setNome(String nomeFilme) {
+               this.nome = nomeFilme;
+           }
+       
+           public int getAutor() {
+               return autor;
+           }
+       
+           public void setAutor(int autorFilme) {
+               this.autor = autorFilme;
+           }
+       
+             
+       
+       }
