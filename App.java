642dbfce9c2faf3884ed9bcfa553f7d7086238c7
neocir17 <71474496+neocir17@users.noreply.github.com>
2020-11-18 21:30:27 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {   
+    
+    public interface Video {
+
+        public void executar();
+        public void pausar();
+        public void mudarAudio();
+        public void mudarLegenda();
+        public void fechar();
+        public String exibirDetalhes();
+        
+
+    }
+}
