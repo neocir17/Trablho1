568e97a65fa7218218cde4ca1f7d23bec3c14e62
neocir17 <71474496+neocir17@users.noreply.github.com>
2020-11-18 21:30:57 -0300

    Create Video.java

---
+++ Video.java
@@ -0,0 +1,53 @@
+public class Video {
+    
+    Filme filme1 = new Filme ("O Poco", 90, 2019);
+    filme1.exibirDetalhes();
+    filme1.executar();
+    filme1.pausar();
+    filme1.executar();
+    filme1.mudarAudio();
+    filme1.mudarAudio();
+    filme1.mudarLegenda();
+    filme1.mudarLegenda();
+    System.out.println();
+
+    System.out.println("");
+    Filme filme2 = new Filme ("Power",115, 2020);
+    System.out.println();
+    filme2.exibirDetalhes();
+    filme2.executar();
+    filme2.pausar();
+    filme2.executar();
+    filme2.mudarAudio();
+    filme2.mudarAudio();
+    filme2.mudarLegenda();
+    filme2.mudarLegenda();
+    System.out.println();
+
+    System.out.println("");
+    Serie serie1 = new Serie ("O Airador", 3, 31, 40, 2018);
+    System.out.println();
+    serie1.exibirDetalhes();
+    serie1.executar();
+    serie1.pausar();
+    serie1.executar();
+    serie1.mudarAudio();
+    serie1.mudarAudio();
+    serie1.mudarLegenda();
+    serie1.mudarLegenda();
+    System.out.println();
+
+    System.out.println("");
+    Serie serie2 = new Serie ("Perdidos no Espaco", 22, 2, 20, 2019);
+    System.out.println();
+    serie2.exibirDetalhes();
+    serie2.executar();
+    serie2.pausar();
+    serie2.executar();
+    serie2.mudarAudio();
+    serie2.mudarAudio();
+    serie2.mudarLegenda();
+    serie2.mudarLegenda();
+}
+    
+}
